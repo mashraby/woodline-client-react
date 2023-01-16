14d52135705108dce3241adcf9a96e343bd9e1d4
mashrabyuldashov <yoldashovmashrab@gmail.com>
2023-01-16 19:30:19 +0500

    added order modal and ordercard and count functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useContext } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import OrderModal from "./Components/OrderModal/OrderModal";
 import SearchBar from "./Components/SearchBar/SearchBar";
+import { OpenModalContext } from "./Contexts/OrderModalContext/OrderModalContext";
 
 function App() {
+  const { isOpenModal, setIsOpenModal } = useContext(OpenModalContext)
+
+  const closeModal = () => {
+    setIsOpenModal(false)
+  }
+
   return (
     <>
       <Header />
       <SearchBar />
+      <OrderModal />
+      <div onClick={() => closeModal()} style={isOpenModal===true ? {"display": "block"} : {"display": "none"}} className="backdrop"></div>
     </>
   );
 }
 
 export default App;
+ 
---
+++ src/Components/OrderModal/OrderModal.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useRef } from "react";
+import { OpenModalContext } from "../../Contexts/OrderModalContext/OrderModalContext";
+import OrderCard from "../OrderCard/OrderCard";
+import "./OrderModal.css";
+
+export default function OrderModal() {
+
+  const { isOpenModal, setIsOpenModal } = useContext(OpenModalContext)
+
+  const closeModal = () => {
+    setIsOpenModal(false)
+  }
+
+  return (
+    <div style={isOpenModal===true ? {"display": "block"} : {"display": "none"}} className="order_modal">
+      <div className="order_modal_header">
+        <div className="heading_wrapper">
+          <p className="order_heading">Order</p>
+          <span className="order_modal_count">4</span>
+        </div>
+        <button onClick={() => closeModal()} className="order_close_btn">x</button>
+      </div>
+
+      <div className="filtering_wrapper">
+        <form action="">
+          <select name="" id="">
+            <option value="">Choose type</option>
+          </select>
+          <select name="" id="">
+            <option value="">Choose model</option>
+          </select>
+        </form>
+      </div>
+
+      <div className="orders_list">
+        <OrderCard /> 
+      </div>
+   
+    </div>
+  );
+}
